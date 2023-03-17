35f493a1693355fd9b8ec646e82c4490607e7b3c
Gonza <gonzalo.october@gmail.com>
2023-03-16 23:44:46 -0300

    update firebase init

--- src/api/initApp.js
+++ src/api/initApp.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const app = initializeApp({
-    apiKey: process.env.REACT_API_KEY,
-    authDomain: process.env.REACT_AUTH_DOMAIN,
-    projectId: process.env.REACT_PROJECT_ID,
-    storageBucket: process.env.REACT_storageBucket,
-    messagingSenderId: process.env.REACT_messagingSenderId,
-    appId: process.env.REACT_APP_ID
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
   });
 
 // Initialize Realtime Database and get a reference to the service
