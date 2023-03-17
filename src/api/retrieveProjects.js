b1adfc891baa2fd71ffb2c2c14ed721ee22a8e63
Gonza <gonzalo.october@gmail.com>
2023-03-16 23:45:11 -0300

    update retrieve projects

---
+++ src/api/retrieveProjects.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { app, database } from "./initApp";
+import { getDatabase, ref, child, get } from "firebase/database";
+
+const retriveProjects = async () => {
+
+ const dbRef = ref(getDatabase());
+
+  return await get(child(dbRef, `projects`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log('value: ', snapshot.val());
+      return (snapshot.val());
+    } else {
+      console.warn("No data available");
+      return false;
+    }})
+  .catch((error) => {
+    console.error(error);
+  });
+}
+
+export default retriveProjects;
