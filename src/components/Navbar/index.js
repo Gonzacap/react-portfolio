083e00f74e3c3e175c5f026e52eb6ad087773f4c
gonzacap <gonzalo.october@gmail.com>
2023-07-20 01:20:07 -0300

    disable img nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,13 +6,13 @@ function Navbar() {
     return (
             <nav className="navbar navbar-dark bg-dark" id="nav">
                 <a className="navbar-brand Brand" href="#main" >
-                    <img 
+                    {/* <img 
                         src="img/img-nav.jpg" 
                         width="30" height="30" 
                         className="d-inline-block align-center m-1" 
                         alt="" 
                         loading="lazy" 
-                    />
+                    /> */}
                     Desarrollador Web Fullstack
                 </a>
             </nav>
