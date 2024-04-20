b7f761e4607dee3987992379c6682903479af53f
gonzacap <gonzalo.october@gmail.com>
2024-04-20 13:54:31 -0300

    update add project

--- src/api/addProjects.js
+++ src/api/addProjects.js
@@ -1,29 +1,26 @@
-/* eslint-disable no-unused-vars */
-import { app, database } from "./initApp";
 import { getDatabase, ref, set, push } from "firebase/database";
 
-const addProjects = ( payload ) => {
+const addProjects = (payload) => {
 
   const projectsRef = ref(getDatabase(), `projects`);
   let newRef = push(projectsRef);
   let newKey = newRef.key;
-  
+
   let newEntity = {
     id: payload?.id || newKey,
     image: payload?.img || '',
     title: payload?.title || 'test',
-    description: payload?.description ||'test',
-    appLink: payload?.appLink || '#',
-    codeLink: payload?.codeLink ||'#',
-    enable: payload?.enable || true,
+    description: payload?.description || 'test',
+    appLink: payload?.appLink || '',
+    codeLink: payload?.codeLink || '',
+    enable: payload?.enable,
   };
 
-  console.log('newEntity', newEntity);
-
   return set(newRef, newEntity)
-    .then((res) => res)
+    .then((res) => true)
     .catch((error) => {
       console.error(error);
+      return false;
     });
 };
 
